8ff64de9d91dfa85865ada8a5441dabe25fac7fa
Carlin Preun <carlinpreun@hotmail.com>
2023-12-14 14:09:02 -0600

    Added basic layout, nav, contacts with icons, fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import linkedinLogo from "./assets/linkedin.png";
+import emailLogo from "./assets/envelope-solid.svg";
+import "./App.css";
+import Nav from "./components/nav/nav";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
+      <main>
+        <h1>Carlin Preun</h1>
+        <Nav />
+        <ul className="contacts">
+          <li>
+            <a href="https://www.linkedin.com/in/carlin-preun" target="_blank">
+              <img src={linkedinLogo} alt="Linkedin" width={25} />
+            </a>
+          </li>
+          <li>
+            <a href="mailto:carlinpreun@hotmail.com">
+              <img src={emailLogo} alt="Email" />
+            </a>
+          </li>
+        </ul>
+      </main>
+
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,9 +47,9 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/nav/nav.jsx
@@ -0,0 +1,17 @@
+import "./nav.css";
+
+export default function Nav() {
+//react router routes
+
+    return (
+        <nav>
+            <ul>
+                <li>PHP</li>
+                <li>NEXT/PostgreSQL</li>
+                <li>React/Express/SQLite</li>
+                <li>AppDynamics</li>
+                <li>Python</li>
+            </ul>
+        </nav>
+    )
+}
